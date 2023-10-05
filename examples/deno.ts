eeb05448cfe6f7c601e3edcc32ab38f67bd2870d
Muhammad Azeez <muhammad@dylibso.com>
2023-10-05 17:01:19 +0300

    use top level statements for deno

--- examples/deno.ts
+++ examples/deno.ts
@@ -1,22 +1,18 @@
 import { createPlugin, ExtismPluginOptions } from '../src/deno/mod.ts'
 
-async function main() {
-    const filename = Deno.args[0] || "wasm/hello.wasm";
-    const funcname = Deno.args[1] || "run_test";
-    const input = Deno.args[2] || "this is a test";
-    const wasm = {
-        path: filename
-    }
-
-    const options = new ExtismPluginOptions()
-        .withConfig("thing", "testing")
-        .withWasi();
-
-    const plugin = await createPlugin(wasm, options);
-
-    const res = await plugin.call(funcname, new TextEncoder().encode(input));
-    const s = new TextDecoder().decode(res.buffer);
-    console.log(s)
+const filename = Deno.args[0] || "wasm/hello.wasm";
+const funcname = Deno.args[1] || "run_test";
+const input = Deno.args[2] || "this is a test";
+const wasm = {
+    path: filename
 }
 
-main();
+const options = new ExtismPluginOptions()
+    .withConfig("thing", "testing")
+    .withWasi();
+
+const plugin = await createPlugin(wasm, options);
+
+const res = await plugin.call(funcname, new TextEncoder().encode(input));
+const s = new TextDecoder().decode(res.buffer);
+console.log(s)
